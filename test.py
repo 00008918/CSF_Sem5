8355365f42587f5de4a2436256eb8ba511529af2
00008918 <wiutstudent00008918@gmail.com>
2020-10-31 13:55:27 +0500

    m

---
+++ test.py
@@ -0,0 +1,23 @@
+
+#TASK 10
+def test():
+    n=0
+    while n<5:
+        n+=1
+        print("*" * n)
+
+    while n>0:
+        n-=1
+        print("*" * n)
+
+test()
+
+
+
+#TASK 9
+color_list = ["red", "green", "white", "black"]
+
+def colors():
+    print(color_list[0], color_list[3])
+
+colors()
